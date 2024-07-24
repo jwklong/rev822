9ab8b099cc20f1c54ae57dfef0b513d26972f529
sussy layers dev <jwklong@gmx.com>
2024-07-24 17:47:46 +0100

    thINGS,,, scary

--- src/js/levelManager.js
+++ src/js/levelManager.js
@@ -13,6 +13,8 @@ export default class LevelManager {
 
     /** @param {string} src */
     async addLevel(src) {
+        const id = path.basename(src)
+
         const parser = new XMLParser({
             ignoreAttributes: false,
             attributesGroupName: "attributes",
@@ -26,23 +28,56 @@ export default class LevelManager {
 
         const xml = parser.parse((await fs.readFile(path.join(src, "level.xml"))).toString())
 
-        new Level(xml.level)
+        this.levels[id] = new Level(xml.level, id)
     }
 
     /** @returns {Level?} */
     get currentLevel() {
-        return this.levels[this.#currentLevel]
+        return this.#currentLevel
+    }
+
+    /** @param {string} id */
+    set currentLevel(id) {
+        this.#currentLevel = Object.assign(Level.prototype, this.levels[id])
     }
 }
 
 class Level {
-    /** @param {} xml  */
-    constructor(xml) {
+    /** @type {Layer[]} */
+    layers = []
+
+    /**
+     * @param {Object} xml
+     * @param {string} id 
+     */
+    constructor(xml, id) {
+        this.id = id
+        this.name = xml.head.name.value
+        this.desc = xml.head.desc.value
+
         //parse dem resources
         for (const [key, value] of Object.entries(xml.resources)) {
             for (const resource of value) {
                 this.addResource(key, resource.attributes.id, path.join(__dirname, "../data", resource.attributes.src))
             }
         }
+
+        //parse dem everythinjg else
+        for (const [key, value] of Object.entries(xml.scene)) {
+            switch (key) {
+                case "layer":
+                    this.layers.push(new Layer(value))
+                    break
+            }
+        }
+    }
+}
+
+class Layer {
+    /** @param {Object} xml */
+    constructor(xml) {
+        this.img = xml.attributes.img
+        this.x = xml.attributes.x
+        this.y = xml.attributes.y
     }
 }
